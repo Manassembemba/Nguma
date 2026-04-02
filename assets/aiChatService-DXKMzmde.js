753678a2587a6f49968d86c244c164f21f1c5529
Manassembemba <Manassembemba@users.noreply.github.com>
2026-04-02 07:54:33 +0000

    deploy: 26b6fb30a61a13408ae0e32f8927e4992a95188f

--- assets/aiChatService-DXKMzmde.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./index-Bwp57zE4.js";const e=async(a,o)=>{try{const{data:r,error:t}=await s.functions.invoke("chat-ai",{body:{conversationId:a,message:o}});if(t)throw t;return r}catch{return{shouldEscalate:!0}}};export{e as callChatAI};
